335a8a095de1554129ebd9d4040e6a7fcfbcbfb3
Keishi Mihara <keishi.mihara@contribe.co.jp>
2023-03-13 18:18:02 +0000

    add: 冒頭文のコンポーネントを追加

---
+++ src/lib/Beginning.svelte
@@ -0,0 +1,30 @@
+<script>
+    import Introduction from "./Introduction.svelte";
+    import BottomArrow from "./BottomArrow.svelte";
+    const message_jp = `ここは日本在住のエンジニア、 \nMIHARA Keishi のポートフォリオサイトです。`.replace(/\n/g, "<br />");
+    const message_en = `This is KEISHI Mihara's portfolio site.`;
+    const caption = `( web + backend + desktop application engineer.  ....I'll do whatever I can. )`;
+    const contents = ``;
+</script>
+
+<div class="beginning">
+    <h4>{@html message_jp}</h4>
+    <h4>{message_en}</h4>
+    <h5>{caption}</h5>
+    <div>{contents}</div>
+    <Introduction />
+    <BottomArrow />
+</div>
+
+<style>
+    h4,
+    h5 {
+        font-family: "Noto Sans JP";
+    }
+    .beginning {
+        background-color: #110011;
+        height: 100vh;
+        /* width: 100%; */
+        padding-left: 1em;
+    }
+</style>
---
+++ src/lib/Introduction.svelte
@@ -0,0 +1,20 @@
+<script>
+  const message_jp = `私の自己紹介、製作物や執筆記事、業務で身に付けたスキルをご案内させていただきます。
+  少しでもご参考になれば、幸いです。`.replace(/\n/g, "<br />");
+
+</script>
+
+<div class="intro">
+    <p>{@html message_jp}
+</div>
+
+<style>
+    p {
+        font-family: "Noto Sans JP";
+
+    }
+    .intro {
+        background-color: #110011;
+        padding-top: 1em;
+    }
+</style>

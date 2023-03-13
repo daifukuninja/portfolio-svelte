8abcd72060b484113938115488f5f9a352afb3f5
Keishi Mihara <keishi.mihara@contribe.co.jp>
2023-03-13 18:19:03 +0000

    add: 自己紹介、スキルアピールコンポーネントの追加

---
+++ src/lib/AboutMe.svelte
@@ -0,0 +1,30 @@
+<script>
+  const message_jp = `MIHARA Keishi は日本のフリーランスWebエンジニア, バックエンドプログラマ, デスクトップアプリケーションエンジニア, 糖質制限者, 会社員, Zenn 執筆者, スノーボーダー, 運転手, 元 Lotus Notes/Domino アプリケーションデータベースエンジニアでExcelマクロ書き。
+  ３児の父でもあり, 家庭においては掃除夫, ならびに高所作業を担当する。
+
+  茨城県水戸市出身, 茨城県水戸市育ち, 埼玉県経験者。株式会社 Contribe 所属。`.replace(/\n/g, "<br />");
+</script>
+
+<div class="about">
+    <h2>About Me</h2>
+    <h4 class="caption">( "Wikipedia" style. )</h4>
+    <p>{@html message_jp}
+</div>
+
+<style>
+    p {
+        font-family: "Noto Sans JP";
+
+    }
+    .about {
+        background-color: #110011;
+        height: 80vh;
+        padding-top: 1em;
+        padding-left: 1em;
+        opacity: 0.9;
+    }
+    .caption {
+        font-size: large;
+        margin-bottom: 2em;
+    }
+</style>
---
+++ src/lib/Skills.svelte
@@ -0,0 +1,27 @@
+<script>
+  const message_jp = `Python, C#を開発言語としたデスクトップアプリケーション, エンドユーザ業務効率化の支援, ツール類の設計ならびに開発経験を豊富に持つ。近年はWebシステムの分野において, フロントエンドおよびサーバーサイド(REST API)の開発でPHP(Laravel), Goの実装経験を積む。
+
+  過去にも .NET(旧 ASP.NET MVC)フレームワークを用いたフロントエンド(HTML, CSS3, JavaScript)からバックエンド(C#, Oracle Database)まで, Webアプリケーション開発をフルスタックで経験。
+
+  幅広い技術分野とエンドユーザコンピューティングの経験を兼ね備えた, ユーザーフレンドリーな提案力, 実装力を強みとする。`.replace(/\n/g, "<br />");
+
+</script>
+
+<div class="about">
+    <h2>Skills</h2>
+    <p>{@html message_jp}
+</div>
+
+<style>
+    p {
+        font-family: "Noto Sans JP";
+
+    }
+    .about {
+        background-color: #110011;
+        height: 80vh;
+        padding-top: 1em;
+        padding-left: 1em;
+        opacity: 0.9;
+    }
+</style>

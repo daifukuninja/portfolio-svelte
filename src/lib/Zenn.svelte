05c7db9588de15d6465026c9f34fe830b7035c32
Keishi Mihara <keishi.mihara@contribe.co.jp>
2023-03-17 02:08:14 +0000

    add: zennの記事コンポーネントのテスト実装

---
+++ src/lib/Zenn.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+    import { getZennFeeds } from "../scripts/getZennFeeds";
+    import type { Root } from "../scripts/getZennFeeds";
+
+    let promise = getZennFeeds<Root>();
+</script>
+
+<div>
+    {#await promise}
+        <p>loading...</p>
+    {:then params}
+        <h4>{params.feed.title}</h4>
+        <!-- {#each params. as article}
+        <h4>{article.title}</h4>
+        <img src={article.thumbnail} alt="thumbnail" />
+        <p>{article.description}</p>
+    {/each} -->
+    {:catch error}
+        <p style="color: red">{error.message}</p>
+    {/await}
+</div>

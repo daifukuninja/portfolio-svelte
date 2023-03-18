33cf8ec25e7dcc70815adc2732f58acaab43d744
Keishi Mihara <keishi.mihara@contribe.co.jp>
2023-03-18 12:35:49 +0000

    update: zennの投稿を取り込むように改善

--- src/lib/Zenn.svelte
+++ src/lib/Zenn.svelte
@@ -5,17 +5,34 @@
     let promise = getZennFeeds<Root>();
 </script>
 
-<div>
+<div id="zenn-container">
+    <h4>Zenn への投稿</h4>
     {#await promise}
         <p>loading...</p>
     {:then params}
-        <h4>{params.feed.title}</h4>
-        <!-- {#each params. as article}
-        <h4>{article.title}</h4>
-        <img src={article.thumbnail} alt="thumbnail" />
-        <p>{article.description}</p>
-    {/each} -->
+        <img src={params.feed.image} alt="" width="50" />
+        <h5><a href={params.feed.link}>@{params.items[0].author}</a></h5>
+        {#each {length: 5} as _, i}
+            <img src={params.items[i].enclosure.link} alt="thumbnail" />
+            <p class="zenn-description">{params.items[i].description}</p>
+            <p class="zenn-pubdate">{params.items[i].pubDate}</p>
+        {/each}
     {:catch error}
         <p style="color: red">{error.message}</p>
     {/await}
+    <a data-theme="dark" data-layers="1,2,5" data-stack-embed="true" href="https://embed.stackshare.io/stacks/embed/4df6e200b1e0eadf2139dca096d50b">StackShare</a>
 </div>
+
+<style>
+    #zenn-container {
+        height: 80vh;
+        background-color: #110011;
+        opacity: 0.9;
+    }
+    .zenn-description {
+        font-size: small;
+    }
+    .zenn-pubdate {
+        font-size: small;
+    }
+</style>

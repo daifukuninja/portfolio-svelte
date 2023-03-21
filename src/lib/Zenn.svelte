42824f73cd645393e62532ad476842834f956883
Keishi Mihara <keishi.mihara@contribe.co.jp>
2023-03-21 18:42:29 +0000

    update: zennコンポーネントのレイアウト改善

--- src/lib/Zenn.svelte
+++ src/lib/Zenn.svelte
@@ -3,36 +3,81 @@
     import type { Root } from "../scripts/getZennFeeds";
 
     let promise = getZennFeeds<Root>();
+    const articleCount = 5;
 </script>
 
 <div id="zenn-container">
-    <h4>Zenn への投稿</h4>
+    <h2>Articles</h2>
+    <div id="articles-caption">
+        <a href="https://zenn.dev/">Zenn</a> で技術記事を書いています。(<a
+            href="https://zenn.dev/daifukuninja">@daifukuninja</a
+        >)<br>
+        触ったことのなかった技術のインプレッション, 日々の仕事の中で得た小さな気付きなどを中心に、なるべくこまめにアウトプットできるよう心がけています。<br>
+        ここでは最新の {articleCount} 件の記事を紹介します。
+    </div>
     {#await promise}
         <p>loading...</p>
     {:then params}
-        <img src={params.feed.image} alt="" width="50" />
-        <h5><a href={params.feed.link}>@{params.items[0].author}</a></h5>
-        {#each {length: 5} as _, i}
-            <img src={params.items[i].enclosure.link} alt="thumbnail" />
-            <p class="zenn-description">{params.items[i].description}</p>
-            <p class="zenn-pubdate">{params.items[i].pubDate}</p>
-        {/each}
+        <div id="articles-container">
+            {#each { length: articleCount } as _, i}
+                <div class="article">
+                    <img src={params.items[i].enclosure.link} alt="thumbnail" />
+                    <p class="zenn-description">
+                        {@html params.items[i].description.replace(
+                            /\n/g,
+                            "<br />"
+                        )}
+                    </p>
+                    <p class="zenn-pubdate">created at {params.items[i].pubDate}</p>
+                </div>
+            {/each}
+        </div>
     {:catch error}
         <p style="color: red">{error.message}</p>
     {/await}
-    <a data-theme="dark" data-layers="1,2,5" data-stack-embed="true" href="https://embed.stackshare.io/stacks/embed/4df6e200b1e0eadf2139dca096d50b">StackShare</a>
+    <h5>powered by Zenn</h5>
 </div>
+<div class="blankbox" />
 
 <style>
     #zenn-container {
-        height: 80vh;
         background-color: #110011;
         opacity: 0.9;
+        padding-left: 1em;
     }
     .zenn-description {
         font-size: small;
+        overflow: hidden;
+        display: -webkit-box;
+        -webkit-box-orient: vertical;
+        -webkit-line-clamp: 5;
     }
     .zenn-pubdate {
         font-size: small;
+    }
+    #articles-container {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        flex-wrap: wrap;
+        width: 100%;
+    }
+    #articles-caption {
+        font-family: "Noto Sans JP";
+        padding-bottom: 1em;
+    }
+    .article {
+        width: 360px;
+    }
+    .blankbox {
+        height: 50vh;
+        background-color: #110011;
+        opacity: 0.9;
+    }
+    h5 {
+        margin-bottom: 0;
+    }
+    p {
+        font-family: 'M PLUS Rounded 1c';
     }
 </style>

467c1cb21ea66d40187c1d80331d95e4f00d9926
Keishi Mihara <keishi.mihara@contribe.co.jp>
2023-03-21 18:48:51 +0000

    add: footerの仮実装

---
+++ src/lib/Footer.svelte
@@ -0,0 +1,47 @@
+<footer class="footer">
+    <ul class="md-flex">
+        <li><a href="#">About</a></li>
+        <li><a href="#">サイトマップ</a></li>
+        <li><a href="#">プライバシーポリシー</a></li>
+    </ul>
+    <p class="copyright">© 2023 Example Inc. All Rights Reserved.</p>
+</footer>
+
+<style>
+    a {
+        color: #999;
+        text-decoration: none;
+    }
+
+    ul {
+        list-style: none;
+        padding: 0;
+    }
+
+    .footer {
+        padding: 2rem;
+        font-size: 15px;
+        color: #999;
+        background: #fff;
+        border-top: 1px solid #e5e7eb;
+    }
+
+    .footer a:hover {
+        color: #000;
+    }
+
+    @media (min-width: 768px) {
+        .footer {
+            display: flex;
+            justify-content: space-between;
+        }
+
+        .md-flex {
+            display: flex;
+        }
+
+        .md-flex li + li {
+            margin-left: 16px;
+        }
+    }
+</style>
